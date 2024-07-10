e2c7f2fa351c2bea609c547254aedee987a69bd9
Nishireisan <andrey240300@gmail.com>
2024-07-10 16:53:08 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,79 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history += args
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if self.number_of_floors >= new_floor:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print('Такого этажа не существует')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        isinstance(other, House)
+        return self.number_of_floors == other.number_of_floors
+
+    def __gt__(self, other):
+        isinstance(other, House)
+        return self.number_of_floors > other.number_of_floors
+
+    def __lt__(self, other):
+        isinstance(other, House)
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        isinstance(other, House)
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ge__(self, other):
+        isinstance(other, House)
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        isinstance(other, House)
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = value + self.number_of_floors
+        return self
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он сотанется в истории')
+
+
+h1 = House('ЖК Эльбрус', number_of_floors=10)
+print(House.houses_history)
+h2 = House('ЖК Акация', number_of_floors=20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', number_of_floors=20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
